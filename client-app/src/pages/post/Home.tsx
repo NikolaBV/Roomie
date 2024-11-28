acb989cb9ec0647a850f194c805eaff87579cfe1
Nikola Valkov <bulgarianmapper64@gmail.com>
2024-11-28 14:12:11 +0200

    Added the skeleton for a singular post

--- client-app/src/pages/post/Home.tsx
+++ client-app/src/pages/post/Home.tsx
@@ -1,19 +1,98 @@
 import { useQuery } from "@tanstack/react-query";
-import agent from "../../api/agent";
 import { useParams } from "react-router-dom";
+import agent from "../../api/agent";
+import { Button, Image } from "antd";
 
 export default function Post() {
   const { id } = useParams();
   const postDetails = useQuery({
-    queryKey: ["postsQuery"],
+    queryKey: ["postsQuery", id],
     queryFn: () => {
       return agent.Posts.details(id as string);
     },
   });
+
   console.log(postDetails.data);
+
   return (
-    <>
-      <h1>Post</h1>
-    </>
+    <div
+      className="container"
+      style={{
+        display: "flex",
+        alignItems: "center",
+
+        minHeight: "100vh",
+        flexDirection: "column",
+        padding: "1rem",
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          maxWidth: "50rem",
+          margin: "1rem 0",
+        }}
+      >
+        <Image
+          src="/assets/plovdiv.webp"
+          style={{
+            width: "100%",
+            height: "auto",
+            borderRadius: "0.5rem",
+          }}
+        />
+      </div>
+      <div
+        id="content"
+        style={{
+          backgroundColor: "var(--light-color)",
+          padding: "1rem",
+          width: "100%",
+          maxWidth: "50rem",
+          borderRadius: "0.5rem",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        <div
+          style={{
+            textAlign: "left",
+          }}
+        >
+          <h2
+            style={{
+              fontSize: "1.5rem",
+              fontWeight: "bold",
+              margin: "0 0 1rem 0",
+              wordBreak: "break-word",
+            }}
+          >
+            {postDetails.data?.title}
+          </h2>
+          <p
+            style={{
+              fontSize: "1rem",
+              margin: "0",
+              lineHeight: "1.5",
+              wordBreak: "break-word",
+            }}
+          >
+            {postDetails.data?.description}
+          </p>
+        </div>
+        <div id="footer">
+          <Button
+            style={{
+              height: "3rem",
+              backgroundColor: "var( --secondary-color)",
+              marginTop: "1rem",
+            }}
+          >
+            <span style={{ color: "white", fontWeight: "600" }}>
+              Send A Request
+            </span>
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 }
--- client-app/src/pages/posts/components/PostCard.tsx
+++ client-app/src/pages/posts/components/PostCard.tsx
@@ -1,5 +1,5 @@
 import { Card, Image } from "antd";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 interface Props {
   id: string;
@@ -9,26 +9,24 @@ interface Props {
 }
 
 export default function PostCard({ id, title, description, isLoading }: Props) {
-  const navigate = useNavigate();
   return (
-    <Card
-      loading={isLoading}
-      onClick={() => {
-        navigate(`/post/${id}`);
-      }}
-      hoverable
-      style={{ width: 300, margin: "2rem" }}
-      cover={
-        <Image
-          preview={false}
-          alt="example"
-          src="/assets/plovdiv.webp"
-          loading={isLoading ? "eager" : "lazy"} // Change the image loading based on the query state
-        />
-      }
-    >
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </Card>
+    <Link to={`/posts/${id}`}>
+      <Card
+        loading={isLoading}
+        hoverable
+        style={{ width: 300, margin: "2rem" }}
+        cover={
+          <Image
+            preview={false}
+            alt="example"
+            src="/assets/plovdiv.webp"
+            loading={isLoading ? "eager" : "lazy"}
+          />
+        }
+      >
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </Card>
+    </Link>
   );
 }
