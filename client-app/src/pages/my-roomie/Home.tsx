c47042237962f44cdb6e79a2b042278347ae1849
Nikola Valkov <bulgarianmapper64@gmail.com>
2025-01-03 17:18:26 +0200

    Begun prototype work on the ui in my roomie page

--- client-app/src/pages/my-roomie/Home.tsx
+++ client-app/src/pages/my-roomie/Home.tsx
@@ -1,7 +1,78 @@
+import { Layout, Menu, MenuProps } from "antd";
+import { Content } from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
+
 export default function MyRoomie() {
+  const items: MenuProps["items"] = [
+    {
+      key: "General",
+      label: "General",
+    },
+    {
+      key: "Property",
+      label: "Property",
+    },
+    {
+      key: "Roomies",
+      label: "Roomies",
+    },
+    {
+      key: "Rent & Utilities",
+      label: "Rent",
+    },
+  ];
   return (
-    <>
-      <h1>My roomie</h1>
-    </>
+    <div>
+      <Layout hasSider>
+        <Sider theme="light">
+          <div className="demo-logo-vertical" />
+          <Menu theme="light" items={items} className="side-bar-menu" />
+        </Sider>
+        <Layout>
+          <Content
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignContent: "center",
+            }}
+          >
+            <div className="grid-container">
+              <div
+                style={{
+                  backgroundColor: "red",
+                  height: "15rem",
+                  width: "25rem",
+                  margin: "1rem",
+                }}
+              ></div>
+              <div
+                style={{
+                  backgroundColor: "red",
+                  height: "15rem",
+                  width: "25rem",
+                  margin: "1rem",
+                }}
+              ></div>
+              <div
+                style={{
+                  backgroundColor: "red",
+                  height: "15rem",
+                  width: "25rem",
+                  margin: "1rem",
+                }}
+              ></div>
+              <div
+                style={{
+                  backgroundColor: "red",
+                  height: "15rem",
+                  width: "25rem",
+                  margin: "1rem",
+                }}
+              ></div>
+            </div>
+          </Content>
+        </Layout>
+      </Layout>
+    </div>
   );
 }
