b70e92013a8bfa418141016b78f72959e9c79043
NikolaBV <bulgarianmapper64@gmail.com>
2025-01-05 13:27:41 +0200

    Added property entity class boilerplate

---
+++ Domain/Enums/ApartmentType.cs
@@ -0,0 +1,11 @@
+namespace Domain.Enums
+{
+    public enum ApartmentType
+    {
+        Studio,
+        OneBedroom,
+        TwoBedroom,
+        ThreeBedroom,
+        Other
+    }
+}
---
+++ Domain/Property.cs
@@ -0,0 +1,17 @@
+
+namespace Domain
+{
+    public class Property
+    {
+        public Guid Id { get; set; }
+        public string Address { get; set; }
+        public ApartmentType ApartmentType { get; set; }
+        public int NumberOfRooms { get; set; }
+        public bool Furnished { get; set; }
+        public decimal Rent { get; set; }
+        public string AdditionalNotes { get; set; }
+        
+        public Guid PostId { get; set; }
+        public Post Post { get; set; }
+    }
+}
